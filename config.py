2519331563c157e438a5d891d622892c29d7f74f
Gulzhigit1 <mamatalievasanbek24@gmail.com>
2024-07-23 18:37:35 +0600

    hw7

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ SMTP_USER = "zalkar621@gmail.com"
 SMTP_PASSWORD = "behv jopn hfct nkv"
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
-
+API_TOKEN = '7084282881:AAEyZuoa5jH98ir26yROousSw0l83z0iSFo'
